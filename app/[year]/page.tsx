422a4564cdbe54dadeb33d5d1b716da7ae5c5f18
Agustín Arenas <72575052+ArenasAgustin@users.noreply.github.com>
2025-02-26 09:23:52 -0300

    Update page.tsx

--- app/[year]/page.tsx
+++ app/[year]/page.tsx
@@ -1,6 +1,6 @@
 import Calendar from "@/components/Calendar";
 
-export default async function Page({ params }: { params: { year: string } }) {
+export default async function Page({ params }: { params: Promise<{ year: string }> }) {
   const { year } = await params;
   return <Calendar initialYear={Number.parseInt(year)} />;
 }
